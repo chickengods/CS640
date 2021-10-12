b53b33a4a4f025723ce4f33773dc598178c6d945
Donelson Graham Berger <dgberger2@wisc.edu>
2021-10-12 17:02:01 -0500

    finished part 2, but it still needs to be tested

--- P2/lab2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ P2/lab2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -4,12 +4,18 @@ import net.floodlightcontroller.packet.Ethernet;
 import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
+import java.util.*;
 
 /**
  * @author Aaron Gember-Jacobson
  */
 public class Switch extends Device
-{	
+{
+	//Data Structure to store MAC addresses
+	//Object list is a 1D array with 2 values: 1. time of entry, 2. port
+	HashMap<Object, List<Object>> addressTable;
+
+
 	/**
 	 * Creates a router for a specific host.
 	 * @param host hostname for the router
@@ -17,6 +23,7 @@ public class Switch extends Device
 	public Switch(String host, DumpFile logfile)
 	{
 		super(host,logfile);
+		this.addressTable = new HashMap<Object, List<Object>>();
 	}
 
 	/**
@@ -29,9 +36,22 @@ public class Switch extends Device
 		System.out.println("*** -> Received packet: " +
                 etherPacket.toString().replace("\n", "\n\t"));
 		
-		/********************************************************************/
-		/* TODO: Handle packets                                             */
-		
-		/********************************************************************/
+		//handle packets
+
+		//add source address to table
+		this.addressTable.put(etherPacket.getSourceMAC(), new Object[]{inIface, System.currentTimeMillis()});
+		//look up dest address
+		Object[] destEntry = this.addressTable.get(etherPacket.getDestinationMAC());
+		//send if valid
+		if (destEntry != null || System.currentTimeMillis() - destEntry[0] <= 15000){
+			sendPacket(etherPacket, destEntry[1]);
+		}
+		else{ // not valid --> broadcast
+			// send packet to each interface that isn't the source
+			for(Iface iface: this.interfaces.values()){
+				if(!iface.equals(inIface)){sendPacket(etherPacket, iface);}
+			}
+		}
+
 	}
 }
